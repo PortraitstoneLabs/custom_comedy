a6d2f7457eccafb86a1ea425c57ca155c9ac56a2
PortraitstoneLabs <khristophe.keen@gmail.com>
2023-12-16 19:40:29 -0800

    Firstish Commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+const poppins = Poppins({ subsets: ['latin'],
+weight: ['400', '500', '600', '700'],
+variable: '--font-poppins', })
+
+export const metadata: Metadata = {
+  title: 'Custom Comedy',
+  description: 'Custom Comedy is a platform for event managment.',
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
